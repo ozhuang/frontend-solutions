acd9f3d48dda575faa080c4c5ff3812be23f4034
ozhuang <ozhuang.95@gmail.com>
2016-10-07 17:23:09 +0800

    Update conditional-execution.js

--- program-structure/conditional-execution.js
+++ program-structure/conditional-execution.js
@@ -1,26 +1,11 @@
 /*
  * Hello World
  * Write a function that greets the user by name, or by saying "Hello, World!" if no name is given.
- 
-Bob answers 'Sure.' if you ask him a question.
-He answers 'Whoa, chill out!' if you yell at him.
-He says 'Fine. Be that way!' if you address him without actually saying anything.
-He answers 'Whatever.' to anything else.
-
  */
 
-/* exports.hello = function (name) {
-  your_answer
-}
-*/
-
-function heyBob (words) {
-  if (words.includes('?')){
-    return 'Sure.'}
-  else if (words.includes('!')){
-    return 'Whoa, chill out!'}
-  else if (words.length == 0){
-    return 'Fine. Be that way!'}
-  else {
-    return 'Whatever.'}
+exports.hello = function (name) {
+  if (!name)
+   return "Hello, World!"
+  else
+   return `Hello, ${name}!`
 }
