3e69906c7a1dace5742307d9c6d91195e8a11206
ozhuang <ozhuang.95@gmail.com>
2016-10-19 21:35:00 +0800

    Update while-and-do-loops.js

--- program-structure/while-and-do-loops.js
+++ program-structure/while-and-do-loops.js
@@ -3,7 +3,7 @@
  * Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
  */
 
-exports.chessBoard = {
+var chessBoard = {
   for(int i = 0; i<8; i++){
    for(int j = 0; j<8; j++){
     if((i+j)%2 == 0)
